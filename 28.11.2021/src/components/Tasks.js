d06ae9ee3d25dff0dfc490e1d2f6ee3d1831d919
Segedu <seggedu20@gmail.com>
2021-11-30 20:36:55 +0200

    28.11.2021

--- 28.11.2021/src/components/Tasks.js
+++ 28.11.2021/src/components/Tasks.js
@@ -1,77 +1,69 @@
 import { Component } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 class Tasks extends Component {
-    state = {
-        tasks:
-            [
-                { taskTitle: "reading", isCompleted: true },
-                { taskTitle: "doctor", isCompleted: false },
-                { taskTitle: "swimming", isCompleted: true },
-                // { taskTitle: "presentation", isCompleted: false }
-            ],
-        input: "",
-        update: "",
+    constructor(props) {
+        super(props);
+        this.state = { tasks: [], newTaskName: "" }
     }
-    addTaskHandler = (e) => { this.setState({ input: e.target.value }) }
-    addTaskBtn = (e) => {
+
+    componentDidMount() {
+        this.setState(
+            this.getAllInCompleteTasks()
+        )
+    }
+
+    getAllInCompleteTasks() {
+        return {
+            tasks:
+                [
+                    { taskTitle: "reading", isCompleted: false, id: 1 }
+                ],
+            newTaskName: "",
+        }
+    }
+
+    addTaskInputHandler = (e) => { this.setState({ newTaskName: e.target.value }) }
+
+    handleAddTask = (e) => {
         e.preventDefault();
-        this.setState(state => {
-            let newTask = {
-                taskTitle: this.state.input,
-                isCompleted: false
-            };
-            const update = state.tasks.push(newTask);
-            return {
-                update,
-                input: ''
-            };
-        });
-    }
-    showUnCompleted = () => {
-        this.setState({ tasks: this.state.tasks.filter((task) => task.isCompleted === false) })
-
-    }
-    updateHandler = (e) => {
-        this.setState({ update: e.target.value })
-        console.log(this.state.update);
+        const newState = {
+            ...this.state,
+            tasks: [...this.state.tasks, { taskTitle: this.state.newTaskName, isCompleted: false, id: uuidv4() }],
+            newTaskName: "",
+        };
+        this.setState(newState);
     }
 
-    // setTimeout(() => {
-    // const tasks = [...this.state.tasks]
-    // tasks.taskTitle = 
-    // this.setState({tasks,[i]:this.state.update})
-    // }, 3000)
-    // this.setState(state => {
-    updateBtn = (update, i) => {
-        const tasks = this.state.tasks
-        tasks.map((task, j) => {
-            if (i === j) {
-                return task.taskTitle = update
-            }
-            return task
-        });
-        this.setState({ tasks: tasks })
+    filterUnComplete = () => {
+        return this.state.tasks.filter((task) => task.isCompleted === false)
     }
 
-    deleteBtn = i => { }
+    setCompleted = (taskId) => {
+        const task = this.state.tasks.find((t) => t.id === taskId)
+        task.isCompleted = true;
+        this.setState({ ...this.state });
+    }
 
+    deleteTask = (taskId) => {
+        const task = this.state.tasks.filter((t) => t.id !== taskId)
+        this.state.tasks.splice(task, 1);
+        this.setState({ ...this.state });
+    }
 
     render() {
         return (
             <div>
-                {this.state.tasks.map((task, i) => {
-                    return <div key={i}><h3>{task.taskTitle}</h3>
-                        <button onClick={this.updateBtn}>update</button>
-                        <button onClick={this.deleteBtn}>delete</button>
-
+                {this.filterUnComplete().map((task) => {
+                    return <div key={task.id}><h3>{task.taskTitle}</h3>
+                        <button onClick={() => this.setCompleted(task.id)}>completed</button>
+                        <button onClick={() => this.deleteTask(task.id)}>delete</button>
                     </div>
                 })}
                 <form>
-                    <input type="text" onChange={this.addTaskHandler} placeholder="new task name" />
-                    <input type="submit" value="Add" disabled={!this.state.input} onClick={this.addTaskBtn} />
+                    <input type="text" onChange={this.addTaskInputHandler} placeholder="new task name" />
+                    <input type="button" value="Add" disabled={!this.state.newTaskName} onClick={this.handleAddTask} />
                 </form>
-                <input type="text" id={this.i} value={this.state.update} onChange={this.updateHandler} placeholder="update" />
-                <button onClick={this.showUnCompleted}>show unCompleted</button>
             </div>)
     }
 }
