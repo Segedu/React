19917a051d050d525cb9014af1bb6c67e331152b
Segedu <seggedu20@gmail.com>
2021-12-12 15:15:35 +0200

    12.12.2021

---
+++ 12.12.2021/singlePageApplication/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login'
+import Register from './pages/Register'
+import PageNotFound from './pages/PageNotFound';
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Link to='/' className='link'>Home</Link><img className='tinkerbell' src="https://cdn-icons-png.flaticon.com/128/1583/1583495.png"></img>
+        <Link to='/Login' className='link'>Login</Link><img className='tinkerbell' src="https://cdn-icons-png.flaticon.com/128/1583/1583495.png"></img>
+        <Link to ='/Register' className='link'>Register</Link>
+        <hr />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/Login' component={Login} />
+          <Route exact path= '/Register' component={Register}/>
+          <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+    </BrowserRouter>);
+}
+export default App;
---
+++ 12.12.2021/singlePageApplication/src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const PageNotFound = () => {
+    return (<div>
+        <img src="https://cdn-icons-png.flaticon.com/128/1583/1583495.png"/>
+        <h1>404</h1>
+        <h1>Page Not Found</h1>
+    </div>)
+}
+export default PageNotFound;
