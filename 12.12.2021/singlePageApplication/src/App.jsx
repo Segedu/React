d4e72b4f70ad648aa59c426464d1121080158f78
Segedu <seggedu20@gmail.com>
2021-12-12 15:16:25 +0200

    12.12.2021

--- 12.12.2021/singlePageApplication/src/App.jsx
+++ 12.12.2021/singlePageApplication/src/App.jsx
@@ -3,7 +3,6 @@ import Home from './pages/Home';
 import Login from './pages/Login'
 import Register from './pages/Register'
 import PageNotFound from './pages/PageNotFound';
-
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -12,13 +11,13 @@ function App() {
       <div className="App">
         <Link to='/' className='link'>Home</Link><img className='tinkerbell' src="https://cdn-icons-png.flaticon.com/128/1583/1583495.png"></img>
         <Link to='/Login' className='link'>Login</Link><img className='tinkerbell' src="https://cdn-icons-png.flaticon.com/128/1583/1583495.png"></img>
-        <Link to ='/Register' className='link'>Register</Link>
+        <Link to='/Register' className='link'>Register</Link>
         <hr />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/Login' component={Login} />
-          <Route exact path= '/Register' component={Register}/>
-          <Route component={PageNotFound}/>
+          <Route exact path='/Register' component={Register} />
+          <Route component={PageNotFound} />
         </Switch>
       </div>
     </BrowserRouter>);
