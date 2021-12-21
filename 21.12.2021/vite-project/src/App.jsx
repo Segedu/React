0d669ccef1201ee4e8eb9f1b0f0a4d875f3e4873
Segedu <seggedu20@gmail.com>
2021-12-21 12:25:00 +0200

    21.12.2021

--- 21.12.2021/vite-project/src/App.jsx
+++ 21.12.2021/vite-project/src/App.jsx
@@ -1,14 +1,12 @@
 import { useRef, useState } from 'react'
 import logo from './logo.svg'
 import './App.css';
-// import { useRef } from 'react';
 import AddBookForm from './components/AddBookForm';
 
 function App() {
-  // const [count, setCount] = useState(0)
   return (
     <div className="App">
-   <AddBookForm/>
+      <AddBookForm />
     </div>
   )
 }
