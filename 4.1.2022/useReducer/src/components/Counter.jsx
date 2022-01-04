60bc1bc0b37c03369c54b37cc49254eefa1cd12d
Segedu <seggedu20@gmail.com>
2022-01-04 18:22:18 +0200

    4.1.2022

---
+++ 4.1.2022/useReducer/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import { useReducer } from 'react';
+import counterReducer from '../reducer/counterReducer';
+
+const Counter = ({ initialState }) => {
+    const [state, dispatch] = useReducer(counterReducer, initialState);
+    return (
+        <>
+            Count: {state.count}
+            <button onClick={() => dispatch({ type: "increment" })}>+</button>
+            <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+        </>
+    );
+}
+
+export default Counter;
